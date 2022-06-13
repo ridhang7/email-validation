a9a2624314febb009a7810b1b89ae68334273f4e
Ridhang Panchal <55426399+ridhang7@users.noreply.github.com>
2022-06-14 00:05:24 +0530

    Update server.js
    adding redirect

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ https.createServer(options, function (req, res) {
  res.writeHead(200);
  res.end("Welcome to Node.js");
 }).listen(443)
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
